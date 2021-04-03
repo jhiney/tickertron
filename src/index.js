4ea58d2036b26ec4b84ce48b39429dfa4454bbbd
Jake Hiney <hiney.jake@outlook.com>
2021-04-03 15:05:23 -0500

    Removed Profile.js from initial build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Profile from './Profile';
 import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -10,7 +9,6 @@ ReactDOM.render(
      <BrowserRouter>
         <div className="App">
           <Route path="/" exact component={App} />
-          <Route path="/Profile" exact component={Profile} />
         </div>
       </BrowserRouter>
   </React.StrictMode>,
