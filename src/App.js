6003381c59cf169fb672389a962b94c7bd6d923f
Jake Hiney <hiney.jake@outlook.com>
2021-04-03 19:53:33 -0500

    Update: Shift content into components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import React from "react";
-import { Link } from "react-router-dom";
+import Ticker from './components/Ticker';
+import CommandBar from './components/CommandBar';
 
- 
 export default class App extends React.Component {
+
+
   render() {
     return (
       <div>
-        <p>Home page</p>
-        <Link to="/profile">Go back to profile</Link>
-        <div>
-        </div>
+      <CommandBar />
+      <Ticker />
       </div>
     );
   }
