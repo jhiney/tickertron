cc4d21df27ce4ca00b70eeb2c125eebb7edf3593
Jake Hiney <hiney.jake@outlook.com>
2021-04-05 11:24:39 -0500

    Update .gitignore

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,10 +1,13 @@
 import React from "react";
+require('dotenv').config()
+
+
 
 export default class Ticker extends React.Component {  
   render() {
     return (
       <div>
-        <p>Home</p>
+        <p>Hello</p>
       </div>
     );
   }
