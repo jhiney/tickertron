14753aba5a60dc4397294ee826d9b753fbc69096
Jake Hiney <hiney.jake@outlook.com>
2021-04-06 09:27:16 -0500

    New: Tickerline
    This component takes the ticker that is passed from the Navbar and displays the current price

--- src/components/Tickerline.js
+++ src/components/Tickerline.js
@@ -1,21 +1,41 @@
 import React from "react";
 require('dotenv').config()
-//const axios = require('axios')
 
 export default class Tickerline extends React.Component {
     static displayName = Tickerline.name;
-    
+
     constructor(props) {
         super(props)
         this.state = {
-          ticker:''
+          ticker:this.props.tickerToUse,
+          stockData:''
         }
+    }
+
+    componentDidMount() {
+      if(this.state.ticker) {
+      const cs = 'https://finnhub.io/api/v1/quote?symbol='+ this.state.ticker + '&token='+process.env.REACT_APP_MY_KEY;
+      fetch(cs)
+        .then(res =>res.json())
+        .then(
+          (result) => {
+            this.setState({
+              stockData:result.c
+            })
+            console.log(this.state.stockData)
+          }
+        )
+        
+      }
     }
 
   render() {
     return (
       <div>
-        <p></p>
+        <ul style={{ listStyleType: "none" }}>
+          <li>Ticker: {this.state.ticker}</li>
+          <li>Current Price: {this.state.stockData}</li>
+        </ul>
       </div>
     );
   }
