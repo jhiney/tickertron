8e428f179f58bc5d9915b46cb904b7709b5f7673
Jake Hiney <hiney.jake@outlook.com>
2021-04-08 09:40:34 -0500

    style: Align electron file.

--- electron/main.js
+++ electron/main.js
@@ -1,25 +1,27 @@
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');   
-const path = require('path');
- 
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
 let mainWindow;
- 
+
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        show: false
-    });
-    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
- 
-    mainWindow.loadURL(startURL);
-    
-    //this removes the top bar
-    mainWindow.removeMenu();
- 
-    mainWindow.once('ready-to-show', () => mainWindow.show());
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+  });
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+
+  //this removes the top bar
+  mainWindow.removeMenu();
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
-app.on('ready', createWindow);
+app.on("ready", createWindow);
