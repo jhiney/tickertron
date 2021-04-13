41ab1231ccaf7a3df68ae08983164ac36dbfe6d6
Jake Hiney <hiney.jake@outlook.com>
2021-04-13 07:49:54 -0500

    feat: Add pervious close and color indicators

--- src/components/Tickerline.js
+++ src/components/Tickerline.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import './style/Tickerline.css'
+import "./style/Tickerline.css";
 require("dotenv").config();
 
 export default class Tickerline extends React.Component {
@@ -12,6 +12,7 @@ export default class Tickerline extends React.Component {
       ticker: this.props.tickerToUse,
       stockPrice: "",
       stockName: "",
+      stockPC: "",
     };
   }
 
@@ -28,8 +29,8 @@ export default class Tickerline extends React.Component {
         .then((result) => {
           this.setState({
             stockPrice: result.c,
+            stockPC: result.pc,
           });
-          //console.log(this.state.stockPrice)
         });
     }
   }
@@ -47,7 +48,6 @@ export default class Tickerline extends React.Component {
           this.setState({
             stockName: result.name,
           });
-          //console.log(this.state.stockPrice)
         });
     }
   }
@@ -62,14 +62,21 @@ export default class Tickerline extends React.Component {
       <div className="tickerline">
         <Row xs={2} md={4} lg={6}>
           <Col>{this.state.ticker.toUpperCase()}</Col>
-          <Col>${this.state.stockPrice}</Col>
+          <Col
+            style={
+              this.state.stockPC < this.state.stockPrice
+                ? { color: "forestgreen" }
+                : { color: "red" }
+            }
+          >
+            ${this.state.stockPrice}
+          </Col>
         </Row>
         <Row xs={2} md={4} lg={6}>
           <Col style={{ color: "gray" }}>{this.state.stockName}</Col>
-          <Col></Col>
+          <Col style={{ color: "gray" }}> PC ${this.state.stockPC}</Col>
         </Row>
       </div>
     );
   }
 }
-//<p> Ticker: {this.state.ticker ? this.state.ticker.toUpperCase() : ""} | Current Price: {this.state.stockPrice} </p>
