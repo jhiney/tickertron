eb2d0170a4bf3a754a42f976bbff0e4bd53d2014
Jake Hiney <hiney.jake@outlook.com>
2021-04-26 11:23:44 -0500

    refactor: App from Class to Function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Ticker from "./components/Ticker";
 import CommandBar from "./components/CommandBar";
 import FooterBar from "./components/FooterBar";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ticker: null,
-      listofTicker: [],
-    };
-  }
-  
-  handleCallback = (commandBarData) => {
-    this.setState({ ticker: commandBarData });
-    this.state.listofTicker.push(commandBarData);
-  };
+function App() {
+	//const [ticker, setTicker] = useState('');
+	const [listOfTicker, setlistOfTicker] = useState([]);
 
-  render() {
-    const tickerlist = this.state.listofTicker;
-    return (
-      <div>
-        <CommandBar tickerCallback={this.handleCallback} />
-        <Ticker
-          //key={tickerlist} ---- uncomment this if you want every line to reload on submit, otherwise it'll just load a new ticker below the current one
-          listofTickers={tickerlist}
-        />
-        <FooterBar />
-      </div>
-    );
-  }
+	const handleCallback = (commandBarData) => {
+    //take the commandBarData and append it to the listOfTicker
+		setlistOfTicker([...listOfTicker, commandBarData.ticker]);
+	};
+
+	return (
+		<div>
+			<CommandBar tickerCallback={handleCallback} />
+			<Ticker
+				//key={tickerlist} ---- uncomment this if you want every line to reload on submit, otherwise it'll just load a new ticker below the current one
+				tickerlist={listOfTicker}
+			/>
+			<FooterBar />
+		</div>
+	);
 }
+
+export default App;
